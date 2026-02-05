8b26ad60fee208ad9711de2f067cd084290d125c
Praveen Kumar <58145557+Praveen163@users.noreply.github.com>
2026-02-05 17:24:50 +0530

    Update threading.py

--- threading.py
+++ threading.py
@@ -27,3 +27,30 @@ thread1.join() #main programe ko roko tab tak ye thread complete nhi hoti
 thread2.join()
 
 print("Both threads completed.")
+
+
+
+
+
+
+
+
+
+
+
+#using lock ......... no context switching at that point...only one thread to enter at a time
+lock = threading.Lock()
+shared_counter = 0
+def increment():
+    global shared_counter
+    for _ in range(100000):
+        lock.acquire()
+        shared_counter += 1
+        lock.release()
+
+#using semaphore ... N threads allow to enter at a time
+sem = threading.Semaphore(3)
+def worker():
+    with sem:
+        # at most 3 threads here
+        do_work()
